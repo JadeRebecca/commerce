e01a6d303460ef692f739042a14c2d8e57a568fb
Jade <dupont.jade@gmail.com>
2020-12-27 10:30:35 +0100

    add models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Categorie, Listing, Bid, Comment, Watchlist
+
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ("id", "description")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Categorie, CategorieAdmin)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
+
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,33 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Categorie(models.Model):
+    description = models.CharField(max_length=64)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    start_at = models.DateField()
+    categorie = models.ForeignKey(Categorie, models.SET_NULL, blank=True, null=True )
+    #img_url
+    active = models.BooleanField()
+
+
+class Bid(models.Model):
+    listing  = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    winner = models.BooleanField()
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True )
+    created_at = models.DateField(auto_now_add=True)
+    Comment = models.TextField()
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
