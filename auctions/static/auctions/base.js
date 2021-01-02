bd0fc088656cb495cea163795c46a8dc6bf600b2
Jade <dupont.jade@gmail.com>
2021-01-02 12:45:16 +0100

    menu change color when highlighted

--- auctions/static/auctions/base.js
+++ auctions/static/auctions/base.js
@@ -1,8 +1,5 @@
 const setCurrentNavBarColor = (navName) => {
     el = document.getElementById(navName)
-    el.style.backgroundColor = "#0069d9";
-    el.childNodes[0].style.color = "#fff";
-    console.log("changement de culeur")
+    el.style.backgroundColor = " #ff3368";
+    el.childNodes[1].style.color = "#fff";  
 }
-
-console.log("js ok")
